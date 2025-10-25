6e71efabdb9cf4777029bbdc585d7d7d1b8c6fda
wyyy5111 <wyyy5111@users.noreply.github.com>
2025-10-25 14:40:20 +0800

    chore: update GH Pages base to /yym2 and router basename

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/yyy2">
+    <BrowserRouter basename="/yym2">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
